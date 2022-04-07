02c27d0a40df36163e8ffdc10a38b7a5f1d93ca9
JP10801 <64243159+JP10801@users.noreply.github.com>
2022-04-07 09:54:08 -0700

    Add files via upload

---
+++ new src/node/Saving.java
@@ -0,0 +1,20 @@
+package node;
+
+public class Saving extends Account{
+
+	private double sBalance;
+	
+	public Saving(String fName, String lName, String dob) {
+		super(fName, lName, dob);
+		
+	}
+	
+	public double getBalance() {
+		return sBalance;
+	}
+	
+	public void setBalance(double balance) {
+		sBalance = balance;
+	}
+
+}
